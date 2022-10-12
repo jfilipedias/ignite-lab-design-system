cdbd212e9a5085d4f723ae43fbfbd0203fada4bd
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-10-12 16:27:41 -0300

    Updates the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./styles/global.css";
 
 export function App() {
-  return <h1 className="font-bold text-5xl text-violet-500">Hello World</h1>;
+  return <h1 className="font-bold text-2xl text-violet-500">Hello World</h1>;
 }
