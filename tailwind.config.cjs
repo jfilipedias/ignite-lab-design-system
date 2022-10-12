c5e1763d798806b71968c69770b4fa4d470bc1a0
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-10-12 20:46:38 -0300

    Updates tailwind colors tokens

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,17 +3,20 @@ module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     colors: {
+      transparent: "transparent",
       black: "#000000",
       white: "#FFFFFF",
-
-      "cyan-500": "#81D8F7",
-      "cyan-300": "#9BE1FB",
-
-      "gray-900": "#121214",
-      "gray-800": "#202024",
-      "gray-400": "#7C7C8A",
-      "gray-200": "#C4C4CC",
-      "gray-100": "#E1E1E6",
+      cyan: {
+        500: "#81D8F7",
+        300: "#9BE1FB",
+      },
+      gray: {
+        900: "#121214",
+        800: "#202024",
+        400: "#7C7C8A",
+        200: "#C4C4CC",
+        100: "#E1E1E6",
+      },
     },
     fontSize: {
       xs: 14,
